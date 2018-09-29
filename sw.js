2e03a07404504762a0684e97538630a3b765942d
Eugene Khristo <marteau9292@gmail.com>
2018-09-29 09:05:01 +0300

    Full blown caching. Static and dynamic. Not oprimized yet.

--- sw.js
+++ sw.js
@@ -1,30 +1,63 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
-    .then(cache => {
+    caches.open(CACHE_STATIC_NAME).then(cache => {
       console.log('[Service Worker] Precaching App Shell...');
-      cache.add('/')
-      cache.add('/index.html')
-      cache.add('/src/js/app.js')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
     })
-  )
+  );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+
+  // just for sure that everything is OK (maybe it is not necessary)
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-    .then(res => {
+    caches.match(event.request).then(res => {
       if (res) {
         return res;
       } else {
-        return fetch(event.request);
+        return fetch(event.request)
+          .then(res => {
+            return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(err => {});
       }
     })
   );
 });
+
+caches.keys().then(keysList => console.log(keysList))
